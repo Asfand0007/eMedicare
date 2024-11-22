3a5d5eecaa1ebfbd18ed2967cbc608d1d506542d
Munnazzar Shahzad <munnazzar0214@gmail.com>
2024-11-22 11:08:04 +0500

    hehe

--- backend/database.sql
+++ backend/database.sql
@@ -15,3 +15,82 @@ CREATE TABLE nurses(
     name VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL 
 );
+
+
+
+CREATE SEQUENCE employeeIDGenerator START WITH 1 INCREMENT BY 1;
+
+create table admins (
+  adminID bigint primary key default NEXTVAL('employeeIDGenerator'),
+  firstName text not null,
+  lastName text not null,
+  email text unique not null,
+  authPassword text not null
+);
+
+create table doctors (
+  employeeID bigint primary key default NEXTVAL('employeeIDGenerator'),
+  firstName varchar(255) not null,
+  lastName varchar(255) not null,
+  email text,
+  phoneNumber varchar(255) not null,
+  speciality text not null,
+  startTime time not null,
+  endTime time not null,
+  authPassword text not null
+);
+
+create table nurses (
+  employeeID bigint primary key default NEXTVAL('employeeIDGenerator'),
+  firstName varchar(255) not null,
+  lastName varchar(255) not null,
+  email text,
+  phoneNumber varchar(255) not null,
+  startTime time not null,
+  endTime time not null,
+  authPassword text not null
+);
+
+create table room (
+  roomNumber bigint primary key generated always AS IDENTITY (START WITH 1 INCREMENT BY 1),
+  capacity integer not null,
+  occupied boolean default false
+);
+
+create table patients (
+  mrID bigint primary key generated always as identity,
+  firstName text not null,
+  lastName text not null,
+  gender text,
+  dateOfBirth timestamp with time zone,
+  admissionDate timestamp with time zone not null,
+  roomNumber bigint references room (roomNumber),
+  doctorID bigint references doctor (employeeID)
+);
+
+CREATE TABLE formula (
+    formulaName TEXT PRIMARY KEY,
+    composition TEXT NOT NULL,
+    patentedBy TEXT NOT NULL
+);
+
+CREATE TABLE medicine (
+    medicineName TEXT PRIMARY KEY,
+    stock INTEGER NOT NULL,
+    formulaName TEXT REFERENCES formula (formulaName)
+);
+
+create table dosage (
+  dosageID bigint primary key generated always as identity,
+  dosage_amount text,
+  patientmrID bigint references patient (mrID),
+  formulaName text references formula (formulaName)
+);
+
+create table dosageTimes(
+  dosageID bigint references dosage(dosageID),
+  time time not null,
+  adminitered boolean default (false)
+);
+
+
